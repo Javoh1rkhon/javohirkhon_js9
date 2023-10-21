b2664fcb8af411dcca2e9e49aeaefcb9ab5a6065
javoh1rkhon <rustamovjavohirkhon@gmail.com>
2023-10-21 15:05:35 +0500

    uy ishi tayyor bo'ldi

---
+++ main.js
@@ -0,0 +1,49 @@
+// let a = prompt("how old are you")
+
+//     function years() {
+//         if (a.trim() !== '') {
+//             if(a.trim() > 18){
+//                 console.log("Welcome");
+//             }
+//             if(a.trim() < 18){
+//                 console.log("Close");
+//             }
+//             if(a.trim() === 0){
+//                 console.log("Garang");
+//             }
+//         } else {
+//             console.log("Дебил");
+//         }
+//     } 
+//     years()
+
+//n2
+
+// let a = prompt("biron narsa yozing?")
+// let b = prompt("yana bitta")
+
+
+// function math() {
+//     if (a.trim() === 0 && b.trim() === 0) {
+//         console.log("digar nomera names");
+//     }
+//     else {
+//         console.log(
+//             Number(a) + Number(b),
+//             a - b,
+//             a / b,
+//             a * b,
+//             a ** b,
+//             a % b
+//         );
+//     }
+// }
+
+// math()
+
+//n3 
+
+let a = prompt("biron nomer yoz")
+let b = prompt("ikkinchi nomerni yozing")
+let d = prompt("uchinchi sini ham")
+
